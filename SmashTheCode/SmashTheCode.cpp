4540d8259100133f54cb530c60de7248714d1808
Michał Nowak <bibbit@wp.pl>
2016-05-08 10:02:10 +0200

    StC: Calculating to moves ahead.

--- SmashTheCode/SmashTheCode.cpp
+++ SmashTheCode/SmashTheCode.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+static const size_t ROTS = 4;
+static const size_t MOVES = 8;
+
 template <typename T, std::size_t N>
 constexpr std::size_t countof(T const (&)[N]) noexcept
 {
@@ -71,6 +74,24 @@ inline int GetGroupBonus(int group)
 
 }
 
+inline int GetColorBonus(int colors)
+{
+    switch (colors)
+    {
+    case 2:
+        return 2;
+    case 3:
+        return 4;
+    case 4:
+        return 8;
+    case 5:
+        return 16;
+    default:
+        return 0;
+    }
+}
+
+
 class Grid
 {
 public:
@@ -79,12 +100,13 @@ public:
 
     Grid()
     {
-        for (auto& row : m_Grid)
-            row.fill(Block::Empty);
+        //for (auto& row : m_Grid)
+        //    row.fill(Block::Empty);
     }
 
     /// Allows direct read access to grid.
-    const array<Block, COLS>& operator[](size_t idx) const { assert(idx < ROWS); return m_Grid[idx]; }
+    //const array<Block, COLS>& operator[](size_t idx) const { assert(idx < ROWS); return m_Grid[idx]; }
+    const Block* operator[](size_t idx) const { assert(idx < ROWS); return m_Grid[idx]; }
 
     /// Gets value of specified grid cell.
     /** If row or col is outside bounds then returns Block::Empty. */
@@ -152,15 +174,104 @@ public:
             Block block = Get(row, col);
 
             if (IsEmpty(block))
-                rate += (ROWS - row);
+                rate += (/*ROWS - */row);
 
             if (IsColor(block))
             {
                 static const int MUL = ROWS*ROWS;
 
-                if (Get(row + 1, col) == block)
-                    rate += 1 * MUL;
+                // .
+                // .
+                // x  x..
+                if (Get(row + 1, col + 0) == block &&
+                    Get(row + 2, col + 0) == block)
+                    rate += 16 * MUL;
+                if (Get(row + 0, col + 1) == block &&
+                    Get(row + 0, col + 2) == block)
+                    rate += 16 * MUL;
+
+                //  .  .    .  .
+                //  .  .   .    .
+                // x    x  x    x
+                if (Get(row + 1, col + 1) == block &&
+                    Get(row + 2, col + 1) == block)
+                    rate += 16 * MUL;
+                if (Get(row + 1, col - 1) == block &&
+                    Get(row + 2, col - 1) == block)
+                    rate += 16 * MUL;
+                if (Get(row + 1, col + 0) == block &&
+                    Get(row + 2, col + 1) == block)
+                    rate += 16 * MUL;
+                if (Get(row + 1, col + 0) == block &&
+                    Get(row + 2, col - 1) == block)
+                    rate += 16 * MUL;
+
+                //  ..  ..     x  x
+                // x      x  ..    ..
+                if (Get(row + 1, col + 1) == block &&
+                    Get(row + 1, col + 2) == block)
+                    rate += 16 * MUL;
+                if (Get(row + 1, col - 1) == block &&
+                    Get(row + 1, col - 2) == block)
+                    rate += 16 * MUL;
+                if (Get(row - 1, col - 1) == block &&
+                    Get(row - 1, col - 2) == block)
+                    rate += 16 * MUL;
+                if (Get(row - 1, col + 1) == block &&
+                    Get(row - 1, col + 2) == block)
+                    rate += 16 * MUL;
+
+                // ..  ..   x  x
+                // x    x  ..  ..
+                if (Get(row + 1, col + 0) == block &&
+                    Get(row + 1, col + 1) == block)
+                    rate += 16 * MUL;
+                if (Get(row + 1, col + 0) == block &&
+                    Get(row + 1, col - 1) == block)
+                    rate += 16 * MUL;
+                if (Get(row - 1, col + 0) == block &&
+                    Get(row - 1, col - 1) == block)
+                    rate += 16 * MUL;
+                if (Get(row - 1, col + 0) == block &&
+                    Get(row - 1, col + 1) == block)
+                    rate += 16 * MUL;
+
+                //// .      .  . x  x .
+                //// . x  x .  .      .
+                //if (Get(row + 0, col - 2) == block &&
+                //    Get(row + 1, col - 2) == block)
+                //    rate += 2 * MUL;
+                //if (Get(row + 0, col + 2) == block &&
+                //    Get(row + 1, col + 2) == block)
+                //    rate += 2 * MUL;
+                //if (Get(row - 0, col - 2) == block &&
+                //    Get(row - 1, col - 2) == block)
+                //    rate += 2 * MUL;
+                //if (Get(row - 0, col + 2) == block &&
+                //    Get(row - 1, col + 2) == block)
+                //    rate += 2 * MUL;
+
+                // ..  ..   x  x
+                //
+                //  x  x   ..  ..
+                if (Get(row + 2, col - 0) == block &&
+                    Get(row + 2, col - 1) == block)
+                    rate += 4 * MUL;
+                if (Get(row + 2, col + 0) == block &&
+                    Get(row + 2, col + 1) == block)
+                    rate += 4 * MUL;
+                if (Get(row - 2, col - 0) == block &&
+                    Get(row - 2, col - 1) == block)
+                    rate += 4 * MUL;
+                if (Get(row - 2, col + 0) == block &&
+                    Get(row - 2, col + 1) == block)
+                    rate += 4 * MUL;
+
+                //     .
+                // x.  x
                 if (Get(row, col + 1) == block)
+                    rate += 1 * MUL;
+                if (Get(row + 1, col) == block)
                     rate += 1 * MUL;
             }
         });
@@ -172,7 +283,7 @@ public:
     int Simulate()
     {
         int blocks_count = 0;
-        int chain_power = 0;
+        int chain_power = -1;
         int group_bonus = 0;
         bool colors[(size_t)Block::Count] = { false };
 
@@ -182,7 +293,7 @@ public:
 
             for_each_row_and_col([&](size_t row, size_t col)
             {
-                if (!IsAtLeastForColorBlocks(row, col))
+                if (!IsAtLeastFourColorBlocks(row, col))
                     return;
 
                 colors[(size_t)Get(row, col)] = true;
@@ -195,26 +306,52 @@ public:
 
             if (blocks_count != prev_blocks_count)
             {
-                chain_power = max(chain_power * 2, 8);
+                Colapse();
+
+                if (chain_power == -1)
+                    chain_power = 0;
+                else if (chain_power == 0)
+                    chain_power = 8;
+                else
+                    chain_power *= 2;
+
                 continue;
             }
 
-        } while (false);
+            break;
 
-        if (blocks_count)
+        } while (true);
+
+        if (blocks_count == 0)
             return 0;
 
-        int color_bonus = 0;
+        int colors_count = 0;
         for (size_t i = 0; i < countof(colors); ++i)
         {
             if (colors[i])
-                ++color_bonus;
+                ++colors_count;
         }
+        int color_bonus = GetColorBonus(colors_count);
 
-        return (10 * blocks_count) * max(chain_power + color_bonus + group_bonus, 0);
+        return (10 * blocks_count) * max(chain_power + color_bonus + group_bonus, 1);
     }
 
-    bool IsAtLeastForColorBlocks(size_t row, size_t col) const
+    void Colapse()
+    {
+        for (size_t col = 0; col < COLS; ++col)
+        {
+            for (size_t row = 0, copy_row = 0; row < ROWS; ++row, ++copy_row)
+            {
+                while (copy_row < ROWS && Get(copy_row, col) == Block::Empty)
+                    ++copy_row;
+
+                if (row != copy_row)
+                    Set(row, col, Get(copy_row, col));
+            }
+        }
+    }
+
+    bool IsAtLeastFourColorBlocks(size_t row, size_t col) const
     {
         Block block = Get(row, col);
         if (!IsColor(block))
@@ -252,6 +389,14 @@ public:
             in >> line; in.ignore();
 
             ReadRow(ROWS - 1 - i, line);
+        }
+    }
+
+    void Read(string (&lines)[ROWS])
+    {
+        for (size_t i = 0; i < ROWS; ++i)
+        {
+            ReadRow(ROWS - 1 - i, lines[i]);
         }
     }
 
@@ -296,9 +441,96 @@ private:
             m_Grid[row][i] = BlockFromChar(line[i]);
     }
 
-    matrix<Block, ROWS, COLS> m_Grid;
+    //matrix<Block, ROWS, COLS> m_Grid;
+    Block m_Grid[ROWS][COLS];
 };
 
+int CalculateMove(const Grid& grid, size_t col, size_t rot, Block(&colorsA)[MOVES], Block(&colorsB)[MOVES], int deep);
+
+int CalculateBestMove(const Grid& grid, Block(&colorsA)[MOVES], Block(&colorsB)[MOVES], int deep)
+{
+    int values[Grid::COLS * ROTS] = { 0 };
+
+    for (size_t col = 0; col < Grid::COLS; ++col)
+    {
+        for (size_t rot = 0; rot < ROTS; ++rot)
+        {
+            values[col * ROTS + rot] = CalculateMove(grid, col, rot, colorsA, colorsB, deep);
+        }
+    }
+
+    return *max_element(begin(values), end(values));
+}
+
+bool AddBlocksToGrid(Grid& calc_grid, size_t col, size_t rot, Block(&colorsA)[MOVES], Block(&colorsB)[MOVES], int deep)
+{
+    if (rot == 0)
+    {
+        return calc_grid.AddBlock(col, colorsA[deep]) &&
+               calc_grid.AddBlock(col + 1, colorsB[deep]);
+    }
+    else if (rot == 1)
+    {
+        return calc_grid.AddBlock(col, colorsA[deep]) &&
+               calc_grid.AddBlock(col, colorsB[deep]);
+    }
+    else if (rot == 2)
+    {
+        return calc_grid.AddBlock(col - 1, colorsB[deep]) &&
+               calc_grid.AddBlock(col, colorsA[deep]);
+    }
+    else //if (rot == 3)
+    {
+        return calc_grid.AddBlock(col, colorsB[deep]) &&
+               calc_grid.AddBlock(col, colorsA[deep]);
+    }
+}
+
+int CalculateMove(const Grid& grid, size_t col, size_t rot, Block(&colorsA)[MOVES], Block(&colorsB)[MOVES], int deep)
+{
+    Grid calc_grid = grid;
+
+    int val = numeric_limits<int>::min();
+
+    bool added = AddBlocksToGrid(calc_grid, col, rot, colorsA, colorsB, deep);
+
+    if (added)
+    {
+        int rate = calc_grid.CalculateRate();
+        int score = calc_grid.Simulate();
+        int deep_bonus = 1;
+        val = rate + (score + (score ? (/*MOVES - */deep) * deep_bonus : 0)) * numeric_limits<short>::max();
+
+        if (deep < 2)//MOVES)
+        {
+            int best_val = CalculateBestMove(calc_grid, colorsA, colorsB, deep + 1);
+            if (best_val > 0)
+                val += best_val;
+            else
+                val = best_val;
+        }
+    }
+
+    return val;
+}
+
+size_t FindBestMove(const Grid& grid, Block (&colorsA)[MOVES], Block(&colorsB)[MOVES])
+{
+    int values[Grid::COLS * ROTS] = { 0 };
+
+    for (size_t col = 0; col < Grid::COLS; ++col)
+    {
+        for (size_t rot = 0; rot < ROTS; ++rot)
+        {
+            values[col * ROTS + rot] = CalculateMove(grid, col, rot, colorsA, colorsB, 0);
+        }
+    }
+
+    auto min_it = max_element(begin(values), end(values));
+    size_t min_idx = distance(begin(values), min_it);
+
+    return min_idx;
+}
 
 /**
 * Auto-generated code below aims at helping you parse
@@ -306,15 +538,61 @@ private:
 **/
 int main()
 {
+    if (0)
+    {
+        string lines[] = {
+            "......",
+            "......",
+            "......",
+            "......",
+            "......",
+            ".2....",
+            ".0....",
+            ".1....",
+            "045...",
+            "345...",
+            "325...",
+            "150000",
+            //"1.....",
+            //"2.....",
+            //"1.....",
+            //"1.....",
+            //"1.....",
+            //"2.....",
+            //"22....",
+            //"53....",
+            //"55....",
+            //"44....",
+            //"315...",
+            //"311..."
+        };
+        Grid MyGrid;
+
+        Block colorsA[MOVES] = { Block::Color5, Block::Color2, Block::Color4, Block::Color1, Block::Color5, Block::Color2, Block::Color2, Block::Color1 };
+        Block colorsB[MOVES] = { Block::Color3, Block::Color4, Block::Color4, Block::Color1, Block::Color3, Block::Color1, Block::Color3, Block::Color5 };
+
+        MyGrid.Read(lines);
+        MyGrid.Print(cerr);
+
+        size_t min_idx = FindBestMove(MyGrid, colorsA, colorsB);
+
+        int col = min_idx / ROTS;
+        int rot = min_idx % ROTS;
+
+        cout << col << ' ' << rot << endl; // "x": the column in which to drop your blocks
+
+        return 0;
+    }
+
     Grid MyGrid;
     Grid OtherGrid;
 
     // game loop
     while (1) {
-        Block colorsA[8] = { Block::Skull };
-        Block colorsB[8] = { Block::Skull };
+        Block colorsA[MOVES] = { Block::Skull };
+        Block colorsB[MOVES] = { Block::Skull };
 
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < MOVES; i++)
         {
             char colA, colB;
             cin >> colA >> colB; cin.ignore();
@@ -329,47 +607,7 @@ int main()
         OtherGrid.Read(cin);
         //OtherGrid.Print(cerr);
 
-        static const size_t ROTS = 4;
-        int values[Grid::COLS * ROTS] = { 0 };
-
-        for (size_t col = 0; col < Grid::COLS; ++col)
-        {
-            for (size_t rot = 0; rot < ROTS; ++rot)
-            {
-                Grid CalcGrid = MyGrid;
-
-                int& val = values[col * ROTS + rot];
-                val = numeric_limits<int>::min();
-
-                bool added = false;
-                if (rot == 0)
-                {
-                    added = CalcGrid.AddBlock(col, colorsA[0]) &&
-                            CalcGrid.AddBlock(col + 1, colorsB[0]);
-                }
-                else if (rot == 1)
-                {
-                    added = CalcGrid.AddBlock(col, colorsA[0]) &&
-                            CalcGrid.AddBlock(col, colorsB[0]);
-                }
-                else if (rot == 2)
-                {
-                    added = CalcGrid.AddBlock(col - 1, colorsB[0]) &&
-                            CalcGrid.AddBlock(col, colorsA[0]);
-                }
-                else //if (rot == 3)
-                {
-                    added = CalcGrid.AddBlock(col, colorsB[0]) &&
-                            CalcGrid.AddBlock(col, colorsA[0]);
-                }
-
-                if (added)
-                    val = CalcGrid.CalculateRate() + CalcGrid.Simulate() * numeric_limits<short>::max();
-            }
-        }
-
-        auto min_it = max_element(begin(values), end(values));
-        size_t min_idx = distance(begin(values), min_it);
+        size_t min_idx = FindBestMove(MyGrid, colorsA, colorsB);
 
         int col = min_idx / ROTS;
         int rot = min_idx % ROTS;
