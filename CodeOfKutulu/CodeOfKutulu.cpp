92236b8fbfd4cc2d02e94227053f15e47e5c2fd9
Michał Nowak <bibbit@wp.pl>
2018-06-16 21:01:37 +0200

    Code of Kutulu: Initial submit
    This is only project and code from CodinGame.

---
+++ CodeOfKutulu/CodeOfKutulu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/**
+ * Survive the wrath of Kutulu
+ * Coded fearlessly by JohnnyYuge & nmahoude (ok we might have been a bit scared by the old god...but don't say anything)
+ **/
+int main()
+{
+    int width;
+    cin >> width; cin.ignore();
+    int height;
+    cin >> height; cin.ignore();
+    for (int i = 0; i < height; i++) {
+        string line;
+        getline(cin, line);
+    }
+    int sanityLossLonely; // how much sanity you lose every turn when alone, always 3 until wood 1
+    int sanityLossGroup; // how much sanity you lose every turn when near another player, always 1 until wood 1
+    int wandererSpawnTime; // how many turns the wanderer take to spawn, always 3 until wood 1
+    int wandererLifeTime; // how many turns the wanderer is on map after spawning, always 40 until wood 1
+    cin >> sanityLossLonely >> sanityLossGroup >> wandererSpawnTime >> wandererLifeTime; cin.ignore();
+
+    // game loop
+    while (1) {
+        int entityCount; // the first given entity corresponds to your explorer
+        cin >> entityCount; cin.ignore();
+        for (int i = 0; i < entityCount; i++) {
+            string entityType;
+            int id;
+            int x;
+            int y;
+            int param0;
+            int param1;
+            int param2;
+            cin >> entityType >> id >> x >> y >> param0 >> param1 >> param2; cin.ignore();
+        }
+
+        // Write an action using cout. DON'T FORGET THE "<< endl"
+        // To debug: cerr << "Debug messages..." << endl;
+
+        cout << "MOVE 1 1" << endl; // MOVE <x> <y> | WAIT
+    }
+}
