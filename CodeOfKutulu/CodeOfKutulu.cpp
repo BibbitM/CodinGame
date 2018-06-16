121e2ff5cc2a5255610ae3e256f70b87b5c551c3
Michał Nowak <bibbit@wp.pl>
2018-06-16 23:18:02 +0200

    CoK: Loading input data into grid.

--- CodeOfKutulu/CodeOfKutulu.cpp
+++ CodeOfKutulu/CodeOfKutulu.cpp
@@ -2,8 +2,50 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <cassert>
 
 using namespace std;
+
+class Grid
+{
+public:
+	enum class Cell
+	{
+		Wall,
+		Spawn,
+		Empty,
+	};
+
+	Grid();
+
+	void Load( istream& in );
+
+	Cell GetCell( int x, int y ) const { return m_cells[ GetIndex( x, y ) ]; }
+	int GetWidth() const { return m_width; }
+	int GetHeight() const { return m_height; }
+
+private:
+	void LoadSize( istream& in );
+	void LoadLine( int lineIdx, istream& in );
+
+	int GetIndex( int x, int y ) const
+	{
+		assert( x >= 0 && x < m_width );
+		assert( y >= 0 && y < m_height );
+		return x + y * m_height;
+	}
+	void SetCell( int x, int y, Cell cell )
+	{
+		m_cells[ GetIndex( x, y ) ] = cell;
+	}
+
+	int m_width;
+	int m_height;
+
+	vector< Cell > m_cells;
+};
+
+ostream& operator << ( ostream& out, const Grid& grid );
 
 /**
  * Survive the wrath of Kutulu
@@ -11,14 +53,10 @@ using namespace std;
  **/
 int main()
 {
-    int width;
-    cin >> width; cin.ignore();
-    int height;
-    cin >> height; cin.ignore();
-    for (int i = 0; i < height; i++) {
-        string line;
-        getline(cin, line);
-    }
+	Grid grid;
+	grid.Load( cin );
+	cerr << grid;
+
     int sanityLossLonely; // how much sanity you lose every turn when alone, always 3 until wood 1
     int sanityLossGroup; // how much sanity you lose every turn when near another player, always 1 until wood 1
     int wandererSpawnTime; // how many turns the wanderer take to spawn, always 3 until wood 1
@@ -45,4 +83,75 @@ int main()
 
         cout << "MOVE 1 1" << endl; // MOVE <x> <y> | WAIT
     }
+}
+
+Grid::Grid()
+	: m_width( 0 ), m_height( 0 )
+{}
+
+void Grid::Load( istream& in )
+{
+	LoadSize( in );
+
+	for ( int i = 0; i < m_height; i++ )
+	{
+		LoadLine( i, in );
+	}
+}
+
+void Grid::LoadSize( istream& in )
+{
+	in >> m_width; in.ignore();
+	in >> m_height; in.ignore();
+
+	m_cells.resize( m_width * m_height );
+}
+
+void Grid::LoadLine( int lineIdx, istream& in )
+{
+	string line;
+	getline( in, line );
+
+	for ( int i = 0; i < m_width; ++i )
+	{
+		switch ( line[ i ] )
+		{
+		case '#':
+			SetCell( i, lineIdx, Cell::Wall );
+			break;
+		case 'w':
+			SetCell( i, lineIdx, Cell::Spawn );
+			break;
+		case '.':
+			SetCell( i, lineIdx, Cell::Empty );
+			break;
+		default:
+			assert( false );
+		}
+	}
+}
+
+ostream& operator << ( ostream& out, const Grid& grid )
+{
+	for ( int x = 0; x < grid.GetWidth(); ++x )
+	{
+		for ( int y = 0; y < grid.GetHeight(); ++y )
+		{
+			switch ( grid.GetCell( x, y ) )
+			{
+			case Grid::Cell::Wall:
+				out << '#';
+				break;
+			case Grid::Cell::Spawn:
+				out << 'w';
+				break;
+			case Grid::Cell::Empty:
+				out << '.';
+				break;
+			}
+		}
+		out << endl;
+	}
+
+	return out;
 }
